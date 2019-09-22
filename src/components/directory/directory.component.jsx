36e5455db5d3bef6d542c09eda1001f1ef0c49e4
shambhuray.sam@gmail.com <shambhuray.sam@gmail.com>
2019-09-22 23:39:05 +0530

    Added a basic routing like: react-router-dom

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps}></MenuItem>
                     ))
                 }
             </div>
